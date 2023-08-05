84fd017ea1188f715ff6e70f60836929afcd5d49
seunghwanYun <asdryzx2@gmail.com>
2023-08-05 22:21:49 +0900

    Add DataTransferService basic frame

---
+++ Trigger/Trigger/Network/DataTransferService.swift
@@ -0,0 +1,31 @@
+//
+//  DataTransferService.swift
+//  Trigger
+//
+//  Created by yun on 2023/08/05.
+//
+
+import Foundation
+
+
+// MARK: - DataTransferService protocol
+
+protocol DataTransferService {
+    
+}
+
+
+// MARK: - DefaultDataTransferService
+
+final class DefaultDataTransferService {
+    
+    
+    
+    
+}
+
+// MARK: - Extension DefaultDataTransferService
+
+extension DefaultDataTransferService: DataTransferService {
+    
+}
