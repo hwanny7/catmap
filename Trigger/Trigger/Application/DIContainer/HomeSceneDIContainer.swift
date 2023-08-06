4f918eebd671f949c268790cdd46d430aad6068b
seunghwanYun <asdryzx2@gmail.com>
2023-08-06 18:17:25 +0900

    Add HomeSceneDIContainer

---
+++ Trigger/Trigger/Application/DIContainer/HomeSceneDIContainer.swift
@@ -0,0 +1,22 @@
+//
+//  HomeSceneDIContainer.swift
+//  Trigger
+//
+//  Created by yun on 2023/08/05.
+//
+
+import UIKit
+
+
+
+final class HomeSceneDIContainer {
+    
+    private let apiDataTransferService: DataTransferService
+    
+    init(apiDataTransferService: DataTransferService) {
+        self.apiDataTransferService = apiDataTransferService
+    }
+    
+    
+    
+}
