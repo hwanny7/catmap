4209919d2af78e8adc093477361da7f721a7d66d
seunghwanYun <asdryzx2@gmail.com>
2023-08-06 16:43:56 +0900

    Add Appcordinator lnit and properties

--- Trigger/Trigger/Application/AppCoordinator.swift
+++ Trigger/Trigger/Application/AppCoordinator.swift
@@ -1,4 +1,30 @@
 
 import UIKit
 
+final class AppFlowCoordinator: Coordinator {
+    
+    var navigationController: UINavigationController
+    private let appDIContainer: AppDIContainer
+    
+    init(
+        navigationController: UINavigationController,
+        appDIContainer: AppDIContainer
+    ) {
+        self.navigationController = navigationController
+        self.appDIContainer = appDIContainer
+    }
+    
+    func start() {
+        
+    }
+    
+    
+}
 
+// MARK: - Create TabBar
+
+extension AppFlowCoordinator {
+    
+}
+
+// MARK: - Create DIContainer and Coordinator
