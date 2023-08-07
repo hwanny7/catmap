67c8a1dddfeab32a7a1d62f170cb37336048536c
seunghwanYun <asdryzx2@gmail.com>
2023-08-07 13:20:45 +0900

    add googleMapViewController and storyboard

---
+++ Trigger/Trigger/Presentation/Home/View/GoogleMapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleMapViewController.swift
+//  Trigger
+//
+//  Created by yun on 2023/08/07.
+//
+
+import UIKit
+
+class GoogleMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
