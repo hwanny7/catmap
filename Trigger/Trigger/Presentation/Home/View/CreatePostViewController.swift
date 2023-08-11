b9e40f9bdf0d8cd83e6aaf7113f19bb5fb6cdd7c
seunghwanYun <asdryzx2@gmail.com>
2023-08-11 13:38:49 +0900

    수정

--- Trigger/Trigger/Presentation/Home/View/CreatePostViewController.swift
+++ Trigger/Trigger/Presentation/Home/View/CreatePostViewController.swift
@@ -16,41 +16,6 @@ class CreatePostViewController: UIViewController, UICollectionViewDelegate, UICo
     
     private var collectionView: UICollectionView!
     
-    var colors: [UIColor] = [
-        .link,
-        .red,
-        .systemPink,
-        .systemBlue,
-        .systemGray,
-        .systemBlue,
-        .systemGray,
-    ]
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        layout.itemSize = CGSize(width: view.frame.size.width/3.2, height: view.frame.size.width/3.2)
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-
-        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView?.backgroundColor = .black
-        view.addSubview(collectionView!)
-
-        let gesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPressGesture))
-        collectionView?.addGestureRecognizer(gesture)
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        collectionView.frame = CGRect(x: 0, y: 100, width: view.frame.size.width, height: view.frame.size.height / 4)
-    }
-    
     
     init(with viewModel: DefaultPostViewModel) {
         self.viewModel = viewModel
@@ -61,53 +26,17 @@ class CreatePostViewController: UIViewController, UICollectionViewDelegate, UICo
         fatalError("init(coder:) has not been implemented")
     }
     
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(ImageCollectionViewController(frame: view.frame))
+    }
+    
+    
+    
 
     // MARK: - Handle gesture
     
-    @objc func handleLongPressGesture(_ gesture: UILongPressGestureRecognizer) {
-
-        switch gesture.state {
-        case .began:
-            guard let targetIndexPath = collectionView.indexPathForItem(at: gesture.location(in: collectionView)) else { return }
-            // User가 다른 곳을 길게 누르는 경우에는 return
-            collectionView.beginInteractiveMovementForItem(at: targetIndexPath)
-        case .changed:
-            collectionView.updateInteractiveMovementTargetPosition(gesture.location(in: collectionView))
-        case .ended:
-            collectionView.endInteractiveMovement()
-        default:
-            collectionView.cancelInteractiveMovement()
-        }
-    }
 
 }
 
-extension CreatePostViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return colors.count
-    }
-
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = colors[indexPath.row]
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.size.width/3.2,
-                      height: collectionView.bounds.size.height)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        let item = colors.remove(at: sourceIndexPath.row)
-        colors.insert(item, at: destinationIndexPath.row)
-
-        // 실제 아이템의 위치도 변경해줘야 그 자리를 유지한다.
-    }
-    
-}
