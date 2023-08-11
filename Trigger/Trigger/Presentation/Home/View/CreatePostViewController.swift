b632c8a51285bf88417020dbf6075fdff713299d
seunghwanYun <asdryzx2@gmail.com>
2023-08-11 13:20:30 +0900

    ADD: Custom Collection View 생성 및 이식 준비

--- Trigger/Trigger/Presentation/Home/View/CreatePostViewController.swift
+++ Trigger/Trigger/Presentation/Home/View/CreatePostViewController.swift
@@ -7,12 +7,14 @@
 
 import UIKit
 
-class CreatePostViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, StoryboardInstantiable {
+class CreatePostViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
     
     private var viewModel: DefaultPostViewModel
     
-    private var collectionView: UICollectionView?
+    private var scrollView: UIScrollView!
+    
+    private var collectionView: UICollectionView!
     
     var colors: [UIColor] = [
         .link,
@@ -20,34 +22,33 @@ class CreatePostViewController: UIViewController, UICollectionViewDelegate, UICo
         .systemPink,
         .systemBlue,
         .systemGray,
+        .systemBlue,
+        .systemGray,
     ]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // TapGesture
-//        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapCameraButton))
-//        cameraButton.addGestureRecognizer(tapGesture)
-        
+
         let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
+        layout.scrollDirection = .horizontal
         layout.itemSize = CGSize(width: view.frame.size.width/3.2, height: view.frame.size.width/3.2)
         layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-        collectionView?.delegate = self
-        collectionView?.dataSource = self
-//        collectionView?.background = .white
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView?.backgroundColor = .black
         view.addSubview(collectionView!)
-        
+
         let gesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPressGesture))
         collectionView?.addGestureRecognizer(gesture)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        collectionView?.frame = view.bounds
+        collectionView.frame = CGRect(x: 0, y: 100, width: view.frame.size.width, height: view.frame.size.height / 4)
     }
     
     
@@ -60,26 +61,11 @@ class CreatePostViewController: UIViewController, UICollectionViewDelegate, UICo
         fatalError("init(coder:) has not been implemented")
     }
     
-    
-    
-//    static func create(
-//        with viewModel: DefaultPostViewModel
-//    ) -> CreatePostViewController {
-//        let view = CreatePostViewController.instantiateViewController()
-//        view.viewModel = viewModel
-//        return view
-//    }
-    
-    @objc func didTapCameraButton() {
-        print("Tap!")
-    }
-    
+
     // MARK: - Handle gesture
     
     @objc func handleLongPressGesture(_ gesture: UILongPressGestureRecognizer) {
-        guard let collectionView = collectionView else {
-            return
-        }
+
         switch gesture.state {
         case .began:
             guard let targetIndexPath = collectionView.indexPathForItem(at: gesture.location(in: collectionView)) else { return }
@@ -110,7 +96,7 @@ extension CreatePostViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.size.width/3.2,
-                    height: view.frame.size.width/3.2)
+                      height: collectionView.bounds.size.height)
     }
     
     func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool {
@@ -120,6 +106,7 @@ extension CreatePostViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         let item = colors.remove(at: sourceIndexPath.row)
         colors.insert(item, at: destinationIndexPath.row)
+
         // 실제 아이템의 위치도 변경해줘야 그 자리를 유지한다.
     }
     
