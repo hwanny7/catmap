c709d0f0425f6297aa4cc99e72d614cea9e5ef2a
seunghwanYun <asdryzx2@gmail.com>
2023-08-19 23:14:02 +0900

    add: Endpoint 추가

--- CatMap/CatMap/Network/DataTransferService.swift
+++ CatMap/CatMap/Network/DataTransferService.swift
@@ -49,6 +49,9 @@ protocol DataTransferService {
     ) -> NetworkCancellable? where E.Response == Void
 }
 
+protocol ResponseDecoder {
+    func decode<T: Decodable>(_ data: Data) throws -> T
+}
 
 // MARK: - DefaultDataTransferService
 
@@ -69,5 +72,79 @@ final class DefaultDataTransferService {
 // MARK: - Extension DefaultDataTransferService
 
 extension DefaultDataTransferService: DataTransferService {
+    func request<T: Decodable, E: ResponseRequestable>(
+        with endpoint: E,
+        on queue: DataTransferDispatchQueue,
+        completion: @escaping CompletionHandler<T>
+    ) -> NetworkCancellable? where E.Response == T {
+        // 이 함수에 들어오기도 전에 T 타입은 결정이 됐다.
+        networkService.request(endpoint: endpoint) { result in
+            switch result {
+            case .success(let data):
+                let result: Result<T, DataTransferError> = self.decode(
+                    data: data,
+                    decoder: endpoint.responseDecoder
+                )
+                queue.asyncExecute { completion(result) }
+            case .failure(let error):
+                self.errorLogger.log(error: error)
+                let error = self.resolve(networkError: error)
+                queue.asyncExecute { completion(.failure(error)) }
+            }
+        }
+    }
     
+    func request<T: Decodable, E: ResponseRequestable>(
+        with endpoint: E,
+        completion: @escaping CompletionHandler<T>
+    ) -> NetworkCancellable? where E.Response == T {
+        request(with: endpoint, on: DispatchQueue.main, completion: completion)
+    }
+
+    func request<E>(
+        with endpoint: E,
+        on queue: DataTransferDispatchQueue,
+        completion: @escaping CompletionHandler<Void>
+    ) -> NetworkCancellable? where E : ResponseRequestable, E.Response == Void {
+        networkService.request(endpoint: endpoint) { result in
+            switch result {
+            case .success:
+                queue.asyncExecute { completion(.success(())) }
+            case .failure(let error):
+                self.errorLogger.log(error: error)
+                let error = self.resolve(networkError: error)
+                queue.asyncExecute { completion(.failure(error)) }
+            }
+        }
+    }
+    // 결과 값이 없이 성공, 실패 여부만 반환하는 경우
+
+    func request<E>(
+        with endpoint: E,
+        completion: @escaping CompletionHandler<Void>
+    ) -> NetworkCancellable? where E : ResponseRequestable, E.Response == Void {
+        request(with: endpoint, on: DispatchQueue.main, completion: completion)
+    }
+
+    // MARK: - Private
+    private func decode<T: Decodable>(
+        data: Data?,
+        decoder: ResponseDecoder
+    ) -> Result<T, DataTransferError> {
+        do {
+            guard let data = data else { return .failure(.noResponse) }
+            let result: T = try decoder.decode(data)
+            return .success(result)
+        } catch {
+            return .failure(.parsing(error))
+        }
+    }
+}
+
+class JSONResponseDecoder: ResponseDecoder {
+    private let jsonDecoder = JSONDecoder()
+    init() { }
+    func decode<T: Decodable>(_ data: Data) throws -> T {
+        return try jsonDecoder.decode(T.self, from: data)
+    }
 }
