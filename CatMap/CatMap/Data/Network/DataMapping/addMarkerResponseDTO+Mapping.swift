91decca50a8acad389b9abb009900d157a8ace25
seunghwanYun <asdryzx2@gmail.com>
2023-08-29 21:39:12 +0900

    add: detail 페이지 entity 추가

---
+++ CatMap/CatMap/Data/Network/DataMapping/addMarkerResponseDTO+Mapping.swift
@@ -0,0 +1,24 @@
+//
+//  addMarkerResponseDTO+Mapping.swift
+//  CatMap
+//
+//  Created by yun on 2023/08/29.
+//
+
+import Foundation
+
+
+struct addMarkerResponseDTO: Decodable {
+    private enum CodingKeys: CodingKey {
+        case content
+        case images
+    }
+    
+    let content: String
+    let images: [String]
+    
+}
+
+extension addMarkerResponseDTO {
+    func toDomain() -> 
+}
