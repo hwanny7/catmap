80c74fd7c95d7e2184763204348a045520ebaba3
seunghwanYun <asdryzx2@gmail.com>
2023-09-13 19:47:00 +0900

    refactor: login response dto, user entity 수정

--- CatMap/Data/Network/DataMapping/LoginResponseDTO+Mapping.swift
+++ CatMap/Data/Network/DataMapping/LoginResponseDTO+Mapping.swift
@@ -9,16 +9,18 @@ import Foundation
 
 struct LoginResponseDTO: Decodable {
     private enum CodingKeys: String, CodingKey {
-        case accessToken
+        case accessToken = "appleAccessToken"
+        case refreshToken = "appleRefreshToken"
         case nickname
     }
     
+    let refreshToken: String
     let accessToken: String
     let nickname: String
 }
 
 extension LoginResponseDTO {
     func toDomain() -> User {
-        return .init(accessToken: accessToken, nickname: nickname)
+        return .init(accessToken: accessToken, nickname: nickname, refreshToken: refreshToken)
     }
 }
--- CatMap/Domain/Entities/User.swift
+++ CatMap/Domain/Entities/User.swift
@@ -11,4 +11,5 @@ import Foundation
 struct User {
     let accessToken: String
     let nickname: String
+    let refreshToken: String
 }
